a841aa5bff9ee19485e00bc919e919bd5966d63c
Wilton <129124024+Wiltonll@users.noreply.github.com>
2025-02-22 16:28:48 -0300

    UP Reducer & Checkout

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom'; 
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render( 
-  <React.StrictMode>
+import "./index.css";
+import "./bootstrap.min.css";
+import store from "./redux/store/store";
+
+ReactDOM.render(
+  <Provider store={store}>
+    
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
+
 
 reportWebVitals();
